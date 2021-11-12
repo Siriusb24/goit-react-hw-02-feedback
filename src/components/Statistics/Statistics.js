409dc53ec0435b04ccc493390e14eb24fe8ec3b7
Siriusb24 <Siriusb2324@gmail.com>
2021-11-12 09:47:57 +0200

    update

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,13 +1,20 @@
+import Notification from "components/Notification/Nitification"
 
-export default function Statistics({good, neutral, bad}){
- 
+
+export default function Statistics({good, neutral, bad, totalCounter, totalPercentage}){
+   const totalFeedback = totalCounter();
+   const positivePercent = totalPercentage();
+
     return(
         <div>
-            <ul>
+            {totalFeedback ?
+            (<ul>
             <li>Good: <span>{good}</span></li>
             <li>Neutral: <span>{neutral}</span></li>
             <li>Bad: <span>{bad}</span></li>
-            </ul>
+            <li>Total: <span>{totalFeedback}</span></li>
+            <li>Positive Feedback: <span>{positivePercent}%</span></li>
+            </ul>) : <Notification message = {'No feedback given'}/>}
         </div>
     )
 }
